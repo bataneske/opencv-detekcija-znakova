918d1e73d94937a178b0d9e123eee9ee49668e5a
NotMyRealGitt <stefan.kurcan22@gmail.com>
2022-07-06 21:33:24 +0200

    fale jos boje tj segmentacija

--- Enums.hpp
+++ Enums.hpp
@@ -4,29 +4,10 @@
 #include "opencv2/opencv.hpp"
 #include <string>
 
-enum HSV_Segmented_Sign_Enum {
-	SEGM_BACKGROUND,
-	SEGM_RED,
-	SEGM_GREEN,
-	SEGM_BLUE,
-	SEGM_YELLOW,
-	SEGM_BLACK,
-	SEGM_WHITE
-};
+using namespace std;
+using namespace cv;
 
-
-static cv::Scalar segm_to_color[] = {
-	cv::Scalar(161, 111, 245),
-	cv::Scalar(0, 0, 255),
-	cv::Scalar(0, 255, 0),
-	cv::Scalar(255, 0, 0),
-	cv::Scalar(0, 255, 255),
-	cv::Scalar(0, 0, 0),
-	cv::Scalar(255, 255, 255)
-};
-
-
-static std::string sign_types[] = {
+static string SignTypes[] = {
 	"Stop",
 	"Priority",
 	"Crosswalk",
@@ -35,9 +16,41 @@ static std::string sign_types[] = {
 	"One-way",
 	"Danger",
 	"No parking",
-	"No priority",
+	"No priority", // == yield
 	"Pedestrian zone",
 	"Highway Entry",
 	"Highway Exit",
 	"Black spot"
+};
+enum ColorsEnum{
+	SEGM_RED,
+	SEGM_GREEN,
+	SEGM_BLUE,
+	SEGM_YELLOW,
+	SEGM_BLACK,
+	SEGM_WHITE,
+	SEGM_BACKGROUND
+};
+// hl1, hh1, hl2, hh2, sl, sh, vl, vh
+// thresholdi se eksperimentalno pronalaze, i zatim beleze
+// fora je da opsezi (low-high) budu minimalni, a da i dalje nadje ceo znak i nista od pozadine
+
+vector<vector<int>> ColorsThresholds{
+	{0, 14, 160, 179, 120, 255, 74, 218}, //RED
+	{81, 92, 81, 92, 95, 255, 74, 134}, //GREEN
+	{104, 113, 104, 113, 183, 255, 117, 244}, //BLUE
+	{20, 28, 20, 28, 89, 255, 116, 255}, //YELLOW
+	{0, 179, 0, 179, 0, 130, 0, 99}, //BLACK
+	{0, 179, 0, 179, 0, 45, 162, 255} //WHITE
+};
+
+//   u  BGR  !!
+vector<Scalar> ColorsBGR{
+	{0, 0, 255}, //RED
+	{0, 255, 0}, //GREEN
+	{255, 0, 0}, //BLUE
+	{0, 255, 255}, //YELLOW
+	{0, 0, 0}, //BLACK
+	{255, 255, 255}, //WHITE
+	{161, 111, 245} //BACKGROUND  (HSV -> 168, 140, 244)
 };
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 using namespace std;
+#include "Enums.hpp"
 
 #include <unistd.h>
 
@@ -13,56 +14,6 @@ int hl1=0, hh1=14, hl2=160, hh2=179, sl=120, sh=255, vl=74, vh=218; // threshold
 int cl=0, ch=0; 		// Canny parameters slider positions
 int dw=5; 				// dilation width slider position
 int ath=5000; 			// threshold for recognising area as contour, also has a slider
-
-static string SignTypes[] = {
-	"Stop",
-	"Priority",
-	"Crosswalk",
-	"Parking",
-	"Proceed straight",
-	"One-way",
-	"Danger",
-	"No parking",
-	"No priority", // == yield
-	"Pedestrian zone",
-	"Highway Entry",
-	"Highway Exit",
-	"Black spot"
-};
-enum ColorsEnum{
-	SEGM_BACKGROUND,
-	SEGM_RED,
-	SEGM_GREEN,
-	SEGM_BLUE,
-	SEGM_YELLOW,
-	SEGM_BLACK,
-	SEGM_WHITE
-};
-// hl1, hh1, hl2, hh2, sl, sh, vl, vh
-// thresholdi se eksperimentalno pronalaze, i zatim beleze
-// fora je da opsezi (low-high) budu minimalni, a da i dalje nadje ceo znak i nista od pozadine
-/*
-vector<vector<int>> ColorsThresholds{
-	{}, //BACKGROUND
-	{0, 14, 160, 179, 120, 255, 74, 218}, //RED
-	{}, //GREEN
-	{}, //BLUE
-	{}, //YELLOW
-	{}, //BLACK
-	{} //WHITE
-};
-*/
-//   u  BGR  !!
-vector<Scalar> ColorsBGR{
-	{161, 111, 245}, //BACKGROUND  (HSV -> 168, 140, 244)
-	{0, 0, 255}, //RED
-	{0, 255, 0}, //GREEN
-	{255, 0, 0}, //BLUE
-	{0, 255, 255}, //YELLOW
-	{0, 0, 0}, //BLACK
-	{255, 255, 255} //WHITE
-};
-
 
 void sliders()
 {
@@ -85,7 +36,7 @@ void sliders()
 	createTrackbar("Contour area th", "Sliders", &ath, 32767);
 }
 
-void getCountours(Mat imgDil, Mat img)
+void getCountours(Mat src, Mat imgDil, Mat img)
 {
 	vector<vector<Point>> contours;
 	vector<Vec4i> hierarchy;
@@ -93,22 +44,79 @@ void getCountours(Mat imgDil, Mat img)
 	findContours(imgDil, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
 	//drawContours(img, contours, -1, Scalar(255, 0, 255), 2);
 
+	Mat maskHSV, maskHSV1, maskHSV2;
 	vector<vector<Point>> contPoly(contours.size());
 	vector<Rect> boundRect(contours.size());
+	vector<Mat> imgsCropped;
 	string objectType;
+	Mat warpMatrix, signWarped;
 
 	for(int i = 0; i < contours.size(); i++)
 	{
 		int area = contourArea(contours[i]);
 		//cout << area << endl;
 
-
+		Mat signCropped, signCroppedHSV, signColored, signCroppedResized, signColoredResized;
 		if(area > ath)
 		{
 			float peri = arcLength(contours[i], true);
 			approxPolyDP(contours[i], contPoly[i], 0.02*peri, true);
 			//cout << contPoly[i].size() << endl; // broj uglova
 			boundRect[i] = boundingRect(contPoly[i]);
+
+			// ovde kropujem, bojim, ubacujem u neki vektor
+			Rect roi(boundRect[i].tl(), boundRect[i].br());
+
+
+			// TAG : iseci visak slike
+
+			// znaci imas signColored koji je BGR i imas signCropped koji je HSV
+			// na signCropped ces traziti boje  ( tako sto maskiras sa opsegom za neku boju )
+			// na signColored ces crtati ( tako sto sve delove koji pripadaju maski obojas tom bojom )
+			signColored = src(roi);
+			cvtColor(signColored, signCropped, COLOR_BGR2HSV);
+
+
+			// prodji kroz sve boje, odradi maskiranje, pa tako i farbanje
+
+			cout << "DEBUG _ _ _ _ _ _ _ _ _ 1" << endl;
+			/*
+			// TAG: Pozadina kao zasebna
+
+			// ako bi prvo ofarbao celu kopiju u BACKGROUND, to bi bilo lakse
+
+			// TAG : Obojiti segmente predefinisanim bojama
+
+			for(int i = 0; i < ColorsThresholds.size(); i++)
+			{
+				inRange(signCropped, Scalar(ColorsThresholds[i][0], ColorsThresholds[i][4], ColorsThresholds[i][6]), Scalar(ColorsThresholds[i][1], ColorsThresholds[i][5], ColorsThresholds[i][7]), maskHSV1);
+				inRange(signCropped, Scalar(ColorsThresholds[i][2], ColorsThresholds[i][4], ColorsThresholds[i][6]), Scalar(ColorsThresholds[i][3], ColorsThresholds[i][5], ColorsThresholds[i][7]), maskHSV2);
+				maskHSV = maskHSV1 + maskHSV2;
+				for(int r=0; r<maskHSV.rows; r++)
+				{
+					for(int c=0; c<maskHSV.cols; c++)
+					{
+						if(maskHSV.at<uint>(r, c) != 0)
+						{
+							//signColored.at<double>(r, c) = ColorsBGR[i];
+							cout << i << " " << maskHSV.at<uint>(r, c) << "       " << r << " - " << c << " "  << ColorsBGR[i] << endl;
+							//signColored.at<Vec3b>(r,c) = Scalar(ColorsBGR[i][0], ColorsBGR[i][1], ColorsBGR[i][2]);
+
+							// uklopi jednu po jednu boju u kopiju kropovane slike
+
+						}
+					}
+				}
+				cout << ColorsBGR[i] << endl;
+			}
+			cout << "DEBUG _ _ _ _ _ _ _ _ _ 7" << endl;
+
+			// stavi te obojane slike u neki vektor obojanih slika
+			//imgsCropped.push_back(signColored);
+
+			*/
+			cout << "DEBUG _ _ _ _ _ _ _ _ _ 8" << endl;
+
 			int objCor = (int)contPoly[i].size();
 			if(objCor == 3)
 			{
@@ -128,16 +136,45 @@ void getCountours(Mat imgDil, Mat img)
 			}
 			else if(objCor > 8)
 			{
-				objectType =="Circle";
+				objectType = "Circle";
 			}
 			else
 			{
 				objectType = "None";
 			}
 
+
+			// TAG : skalirati znakove
+
+			resize(signCropped, signCroppedResized, Size(), 2, 2);
+			resize(signColored, signColoredResized, Size(), 2, 2);
+
+
+			//  TAG : transformisati zakrivljene znakove
+
+			// iz nekropovane bi islo ovako
+			//Point2f srcPoints[4] = { {boundRect[i].x, boundRect[i].y}, {boundRect[i].x+boundRect[i].width, boundRect[i].y}, {boundRect[i].x, boundRect[i].y+boundRect[i].height}, {boundRect[i].x+boundRect[i].width, boundRect[i].y+boundRect[i].height} };
+			// iz kropovane prosto ovako
+			Point2f srcPoints[4] = { {0.0f, 0.0f}, {0.0f+boundRect[i].width, 0.0f}, {0.0f, 0.0f+boundRect[i].height}, {0.0f+boundRect[i].width, 0.0f+boundRect[i].height} };
+			Point2f dstPoints[4] = { {0.0f, 0.0f}, {400, 0.0f}, {0.0f, 400}, {400, 400} };
+
+			warpMatrix = getPerspectiveTransform(srcPoints, dstPoints);
+			warpPerspective(signColored, signWarped, warpMatrix, Point(400, 400));
+
+
+			imshow(to_string(i)+"signCropped", signCropped);
+			imshow(to_string(i)+"signColored", signColored);
+
+			imshow(to_string(i)+"signCroppedResized", signCroppedResized);
+			imshow(to_string(i)+"signColoredResized", signColoredResized);
+
+			imshow(to_string(i)+"signWarped", signWarped);
+
+
 			drawContours(img, contPoly, i, Scalar(255, 0, 255), 2);
 			rectangle(img, boundRect[i].tl(), boundRect[i].br(), Scalar(20, 255, 20), 4);
 			putText(img, objectType, {boundRect[i].x, boundRect[i].y-3}, FONT_HERSHEY_PLAIN, 1.65, Scalar(111, 30, 52), 2.4);
+
 		}
 
 	}
@@ -148,7 +185,7 @@ int main() {
 	if(src.empty()){
 		throw runtime_error("Cannot open image!");
 	}
-	cout << "DEBUG _ _ _ _ _ _ _ _ _ 1" << endl;
+	cout << "DEBUG _ _ _ _ _ _ _ _ _ start" << endl;
 
 	Mat imgHSV, imgBlur, imgCanny, imgDil, imgCont;
 	Mat resultHSV, resultHSV1, resultHSV2, maskHSV, maskHSV1, maskHSV2;
@@ -171,7 +208,7 @@ int main() {
 
 		imgCont = src.clone();
 
-		getCountours(imgDil, imgCont);
+		getCountours(src, imgDil, imgCont);
 
 
 
@@ -190,7 +227,7 @@ int main() {
 	}
 
 
-	cout << "DEBUG _ _ _ _ _ _ _ _ _ 2" << endl;
+	cout << "DEBUG _ _ _ _ _ _ _ _ _ end" << endl;
 
 	return 0;
 }
